7cc48e1b48ef2c88806bb41bf34690e6ea299718
Michael Bouclas <mbouclas@gmail.com>
2021-05-20 14:35:57 +0300

    Debugging

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,6 +1,11 @@
-import {Controller} from "routing-controllers";
+import {Controller, Get} from "routing-controllers";
+import {Service} from "typedi";
 
 @Controller('/')
+@Service()
 export class HomeController {
-
+    @Get('')
+    async home() {
+        return 'Somebody took a wrong turn'
+    }
 }
