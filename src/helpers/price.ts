7920134c73f2efb7b1b37ed2414c491a37c4372d
Michael Bouclas <mbouclas@gmail.com>
2021-05-21 14:18:29 +0300

    fixes

--- src/helpers/price.ts
+++ src/helpers/price.ts
@@ -1,5 +1,5 @@
 export function moneyToDbFormat(input: number): number {
-    return input * 100;
+    return Math.round(input * 100);
 }
 
 export function moneyFromDbFormat(input: number): number {
